b7c3adaf2a28ea625692a1e919e397287685a873
zHAODEN13 <912437251@qq.com>
2022-11-19 11:52:38 +0800

    实现note的3个接口

--- wxcloudrun-flask/wxcloudrun/note/note.py
+++ wxcloudrun-flask/wxcloudrun/note/note.py
@@ -2,11 +2,9 @@ import json
 from datetime import datetime
 from flask import request,Blueprint
 from run import app
-from wxcloudrun.note.notedao import insert_note
-from wxcloudrun.model import dbNote
+from wxcloudrun.note.notedao import insert_note, like_add_1, add_support, delete_support, like_delete_1, delete_content
+from wxcloudrun.model import dbNote, dbSupport
 
-
-# app.register_blueprint(test,url_prefix='/test')
 
 note = Blueprint("note", __name__)
 
@@ -18,36 +16,60 @@ def index():
     """
     return 'note_bp_index'
 
+#为了测试 所有的post请求暂时用get替代
 
-@note.route('/note', methods=['GET', 'POST'])
-def judge_opt():
-    if request.method == 'POST':
-        print(request.json)
-        opt = request.form['opt']
-        if opt == 'upload_user_note':
-            data = {}
-            data['user_id'] = request.form['user_id']
-            data['note_id'] = request.form['note_id']
-            data['image_path'] = request.form['image_path']
-            data['content'] = request.form['content']
+@note.route('/note/operate_note',methods=['GET']) #应该只有post
+def operate_note():
+    #user_id = request.form['user_id']
+    #note_id = request.form['note_id']
+    #choice = request.form['choice']
+    user_id = request.args.get('user_id')
+    note_id = request.args.get('note_id')
+    choice = request.args.get('choice')
+    if choice == "insert":
+        # note的点赞数+1
+        res_note = like_add_1(note_id)
+        # support表增加记录
+        support = dbSupport()
+        support.note_id = note_id
+        support.user_id = user_id
+        res_user = add_support(support)
 
-            return json.dumps(data)
-    elif request.method == 'GET':
-        opt = request.args.get('opt')
-        if opt == 'upload_user_note':
-            data = {}
-            data['user_id'] = request.args.get('user_id')
-            data['note_id'] = request.args.get('note_id')
-            data['image_path'] = request.args.get('image_path')
-            data['content'] = request.args.get('content')
-            note = dbNote()
-            note.note_id = request.args.get('note_id')
-            note.publisher_id = request.args.get('user_id')
-            note.publisher_time = datetime.now()
-            note.content = request.args.get('content')
-            note.photo_path = request.args.get('image_path')
-            res = insert_note(note)
-            return res
-        else:
-            return "note"
+        return res_note + "/n" + res_user
+
+    if choice == "delete":
+        # note的点赞数+1
+        res_note = like_delete_1(note_id)
+        #support表删除记录
+        res_user = delete_support(note_id,user_id)
+        return res_note + "/n" + res_user
+
+@note.route('/note/delete_user_note',methods=['GET']) #应该为post
+def delete_user_note():
+    #user_id = request.form['user_id']
+    #note_id = request.form['note_id']
+    #user_id = request.args.get('user_id')
+    note_id = request.args.get('note_id')
+
+    res = delete_content(note_id)
+
+    return res
+
+
+@note.route('/note/upload_user_note', methods=['GET']) #应该为post
+def upload_user_note():
+    note = dbNote()
+    # data['user_id'] = request.form['user_id']
+    # data['note_id'] = request.form['note_id']
+    # data['image_path'] = request.form['image_path']
+    # data['content'] = request.form['content']
+    note.note_id = request.args.get('note_id')
+    note.publisher_id = request.args.get('user_id')
+    note.publisher_time = datetime.now()
+    note.content = request.args.get('content')
+    note.photo_path = request.args.get('image_path')
+
+    res = insert_note(note)
+
+    return res
 
--- wxcloudrun-flask/wxcloudrun/note/notedao.py
+++ wxcloudrun-flask/wxcloudrun/note/notedao.py
@@ -1,20 +1,72 @@
 import logging
-
+from sqlalchemy import and_
 from sqlalchemy.exc import OperationalError
-
 from wxcloudrun import db
-from wxcloudrun.model import dbNote
+from wxcloudrun.model import dbNote, dbSupport
 
 # 初始化日志
 logger = logging.getLogger('log')
-
 
 def insert_note(dbNote):
     try:
         db.session.add(dbNote)
         db.session.commit()
-        return "success"
+        return "insert_note success"
     except OperationalError as e:
-        return e._message()
+        logger.info("insert_note errorMsg= {} ".format(e))
+
+def like_add_1(note_id):
+    try:
+        dbNote.query.filter(dbNote.note_id == note_id).update({'likes_num':dbNote.likes_num+1})
+        db.session.commit()
+        return "like_add_1 success"
+
+    except OperationalError as e:
+        logger.info("like_add_1 errorMsg= {} ".format(e))
+
+def like_delete_1(note_id):
+    try:
+        dbNote.query.filter(dbNote.note_id == note_id).update({'likes_num': dbNote.likes_num - 1})
+        db.session.commit()
+        return "like_delete_1 success"
+
+    except OperationalError as e:
+        logger.info("like_delete_1 errorMsg= {} ".format(e))
+
+def add_support(dbSupport):
+    try:
+        db.session.add(dbSupport)
+        db.session.commit()
+        return "add_support success"
+    except OperationalError as e:
+        logger.info("add_support errorMsg= {} ".format(e))
+
+def delete_support(note_id,user_id):
+    try:
+        counter = dbSupport.query.filter(and_(dbSupport.note_id == note_id, dbSupport.user_id == user_id)).first()
+        if counter is None:
+            return "failed"
+        else:
+            db.session.delete(counter)
+            db.session.commit()
+            return "delete_support success"
+
+    except OperationalError as e:
+        logger.info("delete_support errorMsg= {} ".format(e))
+
+def delete_content(note_id):
+    try:
+        counter = dbNote.query.filter(dbNote.note_id == note_id).first()
+        if counter is None:
+            return 'failed'
+        else:
+            db.session.delete(counter)
+            db.session.commit()
+            return "delete_content success"
+
+    except OperationalError as e:
+        logger.info("delete_content errorMsg= {} ".format(e))
+        return  'failed'
+
 
 
