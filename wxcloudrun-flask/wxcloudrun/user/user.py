eae63276856fe981a67e8d9f7b06f76c652c69f9
mark-feng1219 <107567721+mark-feng1219@users.noreply.github.com>
2022-11-22 10:10:36 +0800

    Update user.py

--- wxcloudrun-flask/wxcloudrun/user/user.py
+++ wxcloudrun-flask/wxcloudrun/user/user.py
@@ -10,37 +10,41 @@ user = Blueprint("user", __name__, url_prefix= '/user')
 
 @user.route('/code', methods=['GET','POST'])
 def user_wxlogin():
-    appid = APPID  # 开发者关于微信小程序的appid
-    appsecret = SECRET  # 开发者关于微信小程序的appsecret
+    try:
+        appid = APPID  # 开发者关于微信小程序的appid
+        appsecret = SECRET  # 开发者关于微信小程序的appsecret
 
-    iv = request.values.get('iv')   # 将前端json数据转为字典
-    code =  request.values.get('code')  # 前端post过来的微信临时登录凭证code
-    encrypteddata = request.values.get('encrypteddata')
+        iv = request.values.get('iv')   # 将前端json数据转为字典
+        code =  request.values.get('code')  # 前端post过来的微信临时登录凭证code
+        encrypteddata = request.values.get('encrypteddata')
 
-    req_params = {
-        'appid': appid,
-        'secret': appsecret,
-        'js_code': code,
-        'grant_type': 'authorization_code'
-    }
-    wx_login_api = 'https://api.weixin.qq.com/sns/jscode2session'
+        req_params = {
+            'appid': appid,
+            'secret': appsecret,
+            'js_code': code,
+            'grant_type': 'authorization_code'
+        }
+        wx_login_api = 'https://api.weixin.qq.com/sns/jscode2session'
 
-    response_data = requests.get(wx_login_api, params=req_params)# 向api发起get请求
+        response_data = requests.get(wx_login_api, params=req_params)# 向api发起get请求
 
-    resdata = response_data.json()
+        resdata = response_data.json()
 
-    openid = resdata['openid']  # 得到用户关于当前小程序的openid
-    session_key = resdata['session_key']  # 得到用户关于当前小程序的会话密钥session_key
-    pc = WXBizDataCrypt(appid, session_key)  # 对用户信息进行解密
-    userinfo = pc.decrypt(encrypteddata, iv)  # 获得用户信息
+        openid = resdata['openid']  # 得到用户关于当前小程序的openid
+        session_key = resdata['session_key']  # 得到用户关于当前小程序的会话密钥session_key
+        pc = WXBizDataCrypt(appid, session_key)  # 对用户信息进行解密
+        userinfo = pc.decrypt(encrypteddata, iv)  # 获得用户信息
 
-    user = dbUser()
-    user.user_id = openid
-    user.user_name = list(userinfo.values())[0]
-    user.user_sex = '男'if list(userinfo.values())[1]==0 else '女'
-    res = create_user(user)
+        user = dbUser()
+        user.user_id = openid
+        user.user_name = list(userinfo.values())[0]
+        user.user_sex = '男'if list(userinfo.values())[1]==0 else '女'
+        res = create_user(user)
 
-    return res
+        return res
+    
+    except Exception as err:
+        return err
 
 
 
