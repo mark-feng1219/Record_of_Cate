ffb21b124670c3dee5082839975e296f6bee3b93
SlackerC <102022891+SlackerC@users.noreply.github.com>
2022-11-23 01:02:26 +0800

    Update config.py

--- wxcloudrun-flask/config.py
+++ wxcloudrun-flask/config.py
@@ -4,8 +4,8 @@ import os
 DEBUG = True
 
 # 读取数据库环境变量
-username = os.environ.get('MYSQL_USERNAME','test')
-password = os.environ.get('MYSQL_PASSWORD','test123!')
-db_address = os.environ.get('MYSQL_ADDRESS','sh-cynosdbmysql-grp-fe9hll88.sql.tencentcdb.com:27015')
+username = os.environ.get('MYSQL_USERNAME')
+password = os.environ.get('MYSQL_PASSWORD')
+db_address = os.environ.get('MYSQL_ADDRESS')
 APPID = "wx9acd048867e8aee8"
 SECRET = "1e74f746f419d6233288968cb00b0783"
