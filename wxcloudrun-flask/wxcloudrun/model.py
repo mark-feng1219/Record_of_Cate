4b56827fca2c6c53d12dd39a415d1ee78244c3e4
zHAODEN13 <912437251@qq.com>
2022-11-23 18:26:31 +0800

    model修改

--- wxcloudrun-flask/wxcloudrun/model.py
+++ wxcloudrun-flask/wxcloudrun/model.py
@@ -6,7 +6,7 @@ from wxcloudrun import db
 class dbNote(db.Model):
     __tablename__ = 'note'
     note_id = db.Column('note_id', db.String(30), nullable=False, primary_key=True)
-    publisher_id = db.Column('publisher_id', db.String(20), nullable=False)
+    publisher_id = db.Column('publisher_id', db.String(50), nullable=False)
     publisher_time = db.Column('publish_time', db.TIMESTAMP, nullable=False, default=datetime.now())
     content = db.Column('content', db.Text, nullable=False)
     photo_path = db.Column('photo_path', db.Text, nullable=False)
@@ -17,7 +17,7 @@ class dbNote(db.Model):
 
 class dbUser(db.Model):
     __tablename__ = 'user_info'
-    user_id = db.Column('user_id', db.String(100), nullable=False, primary_key=True)
+    user_id = db.Column('user_id', db.String(50), nullable=False, primary_key=True)
     user_name = db.Column('user_name', db.String(20), nullable=False)
     head_image_path = db.Column('head_image_path', db.String(255), nullable=False,default='')
     user_sex = db.Column('user_sex', db.String(4), default='保密')
@@ -30,7 +30,7 @@ class dbUser(db.Model):
 class dbComment(db.Model):
     __tablename__ = 'comments'
     comment_id = db.Column('comment_id', db.String(20), nullable=False, primary_key=True)
-    comment_publisher_id = db.Column('comment_publisher_id', db.String(20), nullable=False, primary_key=True)
+    comment_publisher_id = db.Column('comment_publisher_id', db.String(50), nullable=False, primary_key=True)
     comment_publish_time = db.Column('comment_publish_time', db.TIMESTAMP, nullable=False, default=datetime.now())
     comment_content = db.Column('comment_content', db.Text, nullable=True)
     publishAt_note_id = db.Column('publishAt_note_id', db.String(20), nullable=False)
@@ -38,14 +38,14 @@ class dbComment(db.Model):
 
 class dbFollow(db.Model):
     __tablename__ = 'follow'
-    blogger_id = db.Column('blogger_id', db.String(20), nullable=False, primary_key=True)
-    fans_id = db.Column('fans_id', db.String(20), nullable=False, primary_key=True)
+    blogger_id = db.Column('blogger_id', db.String(50), nullable=False, primary_key=True)
+    fans_id = db.Column('fans_id', db.String(50), nullable=False, primary_key=True)
 
 
 class dbSupport(db.Model):
     __tablename__ = 'user_likes_note'
-    note_id = db.Column('note_id', db.String(20), nullable=False, primary_key=True)
-    user_id = db.Column('user_id', db.String(20), nullable=False, primary_key=True)
+    note_id = db.Column('note_id', db.String(30), nullable=False, primary_key=True)
+    user_id = db.Column('user_id', db.String(50), nullable=False, primary_key=True)
     like_time = db.Column('like_time', db.TIMESTAMP, nullable=False, default=datetime.now())
 
 
