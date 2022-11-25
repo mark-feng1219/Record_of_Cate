b8fa6e1c89f435ecc2801b93ef2b7294ae00ff29
mark-feng1219 <1632192407@qq.com>
2022-11-25 22:22:48 +0800

    突然想到要是用户刚发表完笔记或者刚点赞想看,我加入了下拉刷新动作

--- Record_of_Cate/pages/bj/bj.js
+++ Record_of_Cate/pages/bj/bj.js
@@ -13,7 +13,8 @@ Page({
     image_array:[],
     image_like_array:[],
     note_id_like_array:[],
-    title_like_array:[]
+    title_like_array:[],
+    request_count:0
   },
     getUrl: function (e) {
       console.log(this.data.note_id_array)
@@ -63,7 +64,7 @@ titleClick: function (e) {
     url: 'https://flask-ddml-18847-6-1315110634.sh.run.tcloudbase.com/note/mynote',
     data: {user_id:"test_id"},
     header: { 'content-type': 'application/json' },
-    success: (res) =>{resolve(res);console.log(res)},
+    success: (res) =>{resolve(res);console.log('获取笔记数据：',res)},
     fail: function() {console.log('failure')},
     })})
   },
@@ -75,7 +76,7 @@ titleClick: function (e) {
     url: 'https://flask-ddml-18847-6-1315110634.sh.run.tcloudbase.com/support/like_note_info',
     data: {user_id:"test_id"},
     header: { 'content-type': 'application/json' },
-    success: (res) =>{resolve(res);console.log(res)},
+    success: (res) =>{resolve(res);console.log('获取点赞数据',res)},
     fail: function() {console.log('failure')},
     })})
   },
@@ -91,8 +92,7 @@ titleClick: function (e) {
    */
   onShow() {    //避免用户第一步先点开笔记,再登录,再点开笔记没有任何的内容
     console.log('onShow：',app.globalData.login_state)
-    if(app.globalData.login_state==1){  //如果用户处于登录状态
-        wx.cloud.init()
+    if(app.globalData.login_state==1&&this.data.request_count==0){  //如果用户处于登录状态
         this.request_note().then(async(res)=>{
           this.setData({
             image_array:res.data['photo_path'],
@@ -128,6 +128,7 @@ titleClick: function (e) {
           }
           this.setData({card_like_Teams:this.data.card_like_Teams})
         })
+        this.setData({request_count:1})
     }
   },
 
@@ -149,7 +150,43 @@ titleClick: function (e) {
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh() {
+    this.request_note().then(async(res)=>{        //用户刚发表完笔记想看结果
+      this.setData({
+        cardTeams:[],
+        image_array:res.data['photo_path'],
+        note_id_array:res.data['note_id'],
+        title_array:res.data['title']
+      })
+      for(var j=0;j<this.data.note_id_array.length;j++){
+        var tmp_dict={}
+        tmp_dict['imgsrc'] = this.data.image_array[j]
+        tmp_dict['Head_picture'] = app.globalData.user_image_path
+        tmp_dict['count'] = app.globalData.user_name,
+        tmp_dict['name'] = this.data.title_array[j]
+        tmp_dict['note_id'] = this.data.note_id_array[j]
+        this.data.cardTeams.push(tmp_dict)
+      }
+      this.setData({cardTeams:this.data.cardTeams})
+    })
 
+    this.request_like().then(async(res)=>{       //用户刚点赞完笔记想看结果
+      this.setData({
+        card_like_Teams:[],
+        image_like_array:res.data['photo_path'],
+        note_id_like_array:res.data['note_id'],
+        title_like_array:res.data['title']
+      })
+      for(var j=0;j<this.data.note_id_like_array.length;j++){
+        var tmp_dict={}
+        tmp_dict['imgsrc'] = this.data.image_like_array[j]
+        tmp_dict['Head_picture'] = app.globalData.user_image_path
+        tmp_dict['count'] = app.globalData.user_name
+        tmp_dict['name'] = this.data.title_like_array[j]
+        tmp_dict['note_id'] = this.data.note_id_like_array[j]
+        this.data.card_like_Teams.push(tmp_dict)
+      }
+      this.setData({card_like_Teams:this.data.card_like_Teams})
+    })
   },
 
   /**
