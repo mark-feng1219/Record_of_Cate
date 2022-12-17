63ab89fc7f9661f5c418ab852f737be51cdd043e
mark-feng1219 <1632192407@qq.com>
2022-12-17 21:28:12 +0800

    把appid和secret传给前端让前端去访问,后台的appid以环境变量获取

--- wxcloudrun-flask/config.py
+++ wxcloudrun-flask/config.py
@@ -7,5 +7,5 @@ DEBUG = True
 username = os.environ.get('MYSQL_USERNAME', 'test')
 password = os.environ.get('MYSQL_PASSWORD', 'test123!')
 db_address = os.environ.get('MYSQL_ADDRESS', "sh-cynosdbmysql-grp-fe9hll88.sql.tencentcdb.com:27015")
-APPID = "wx9acd048867e8aee8"
-SECRET = "1e74f746f419d6233288968cb00b0783"
+APPID = os.environ.get('APPID')
+SECRET = os.environ.get('SECRET')
--- wxcloudrun-flask/wxcloudrun/user/user.py
+++ wxcloudrun-flask/wxcloudrun/user/user.py
@@ -1,13 +1,14 @@
-from flask import request,Blueprint
+from flask import request, Blueprint
 import json
+import config
 import logging
 from wxcloudrun.model import dbUser
 from wxcloudrun.user.WXBizDataCrypt import WXBizDataCrypt
 from wxcloudrun.user.user_function import update_user_info, create_user, search_id
 
-user = Blueprint("user", __name__, url_prefix= '/user')
+user = Blueprint("user", __name__, url_prefix='/user')
 
-logger = logging.getLogger('log') #初始化日志
+logger = logging.getLogger('log')  # 初始化日志
 
 
 @user.route('/login', methods=['POST'])
@@ -24,7 +25,7 @@ def user_wxlogin():
             user = dbUser()  # 创建新的用户
             user.user_id = openid
             user.user_name = user_name
-            user.user_sex = '男'if user_sex==0 else '女'
+            user.user_sex = '男' if user_sex == 0 else '女'
             user.head_image_path = head_image_path
 
             res = create_user(user)  # 数据添加到数据库中
@@ -33,9 +34,9 @@ def user_wxlogin():
         else:
             res = {}
             name = {'user_name': search_res.user_name}
-            sex = {'user_sex':search_res.user_sex}
-            head = {'user_head':search_res.head_image_path}
-            motto = {'motto':search_res.user_motto}
+            sex = {'user_sex': search_res.user_sex}
+            head = {'user_head': search_res.head_image_path}
+            motto = {'motto': search_res.user_motto}
             res.update(name)
             res.update(sex)
             res.update(head)
@@ -45,6 +46,11 @@ def user_wxlogin():
 
     except Exception as err:
         return err
+
+
+@user.route('/secret', methods=['GET'])
+def get_secret():
+    return {'appid': config.APPID, 'secret': config.SECRET}
 
 
 @user.route('/modify', methods=['POST'])
@@ -58,10 +64,9 @@ def user_modify():
     user_sex = request.json.get('user_sex')
     user_motto = request.json.get('user_motto')
 
-
-    #进行更新
+    # 进行更新
     res = update_user_info(user_id, head_image_path, user_name, user_sex, user_motto)
 
-    #返回状态
+    # 返回状态
     status = {"status": res}
     return json.dumps(status)
